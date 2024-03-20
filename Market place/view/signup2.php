1c7a8865cac43bd5e2bbe41546aeba4402030910
MD.-ALAMIN-HOSSEN <99692491+MD-ALAMIN-HOSSEN@users.noreply.github.com>
2024-03-20 12:49:28 +0600

    Add files via upload

--- Market place/view/signup2.php
+++ Market place/view/signup2.php
@@ -1,3 +1,65 @@
+<?php
+ $fname=$lname=$pass=$confirmpass=$forgotphrase=$country=$email=$err_fname=$err_lname=$err_pass=$err_confirmpass=$err_forgotphrase=$err_country=$err_email="";
+ $error=false;
+ 
+ if(isset($_POST['submit'])){
+    $fname=$_POST['First_Name'];
+    $lname=$_POST['Last_Name'];
+    $pass=$_POST['password'];
+    $confirmpass=$_POST['confirm_password'];
+    $forgotphrase=$_POST['forgot_password_phrase'];
+    $country=$_POST['Country'];
+    $email=$_POST['email'];
+
+    if($fname==""){
+        $err_email="Give mail ";
+        $error=true;
+        
+    }
+    if($lname==""){
+      $err_email="Give mail ";
+      $error=true;
+      
+  }
+  if($pass==""){
+    $err_email="Give mail ";
+    $error=true;
+    
+}
+if($confirmpass==""){
+  $err_email="Give mail ";
+  $error=true;
+  
+}
+if($forgotphrase==""){
+  $err_email="Give mail ";
+  $error=true;
+  
+}
+if($country==""){
+  $err_email="Give mail ";
+  $error=true;
+  
+}
+if($email==""){
+  $err_email="Give mail ";
+  $error=true;
+  
+}
+
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $err_email="Invalid email format.";
+        $error=true;
+    }
+    if(!$error){
+        header('location: ../controller/singupCheck.php');
+    }
+    if($error){
+      echo "please fill the form";
+  }
+    
+ }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -27,79 +89,81 @@
         margin: auto;
         padding: 30px;
     }
+
     .form-group {
         padding: 3px;
-        display: block; 
-    }
-    .center {
-        text-align: center; 
-    
-    }
-    
-    label {
-        font-size: 20px; 
+        display: block;
     }
 
-    button {      
-        font-size: 20px; 
+    .center {
+        text-align: center;
+
+    }
+
+    label {
+        font-size: 20px;
+    }
+
+    button {
+        font-size: 20px;
     }
     </style>
 </head>
 
 <body>
-    <form action="../controller/singupCheck.php" method="post">
+    <form action="#" method="post">
         <div class="form-group center">
-          <h1>Signup</h1>    
+            <h1>Signup</h1>
         </div>
         <div class="form-group">
-          <label for="name">First Name:</label>
-          <input type="text" name="First_Name"  required>
-          <label for="name">Last Name:</label>
-          <input type="text" name="Last_Name"  required>
+            <label for="name">First Name:</label>
+            <input type="text" name="First_Name">
+            <label for="name">Last Name:</label>
+            <input type="text" name="Last_Name">
         </div>
         <div class="form-group">
-          <label for="email">Email:</label>
-          <input type="email" name="email"  required>
-        </div>
-        
-        <div class="form-group">
-          <label for="password">Password:</label>
-          <input type="password" name="password"  required>
-        </div>
-        <div class="form-group">
-          <label for="confirm_password">Confirm Password:</label>
-          <input type="password" name="confirm_password"  required>
-        </div>
-        <div class="form-group">
-          <label for="forgot_password_phrase">Forgot password phrase:</label>
-          <input type="text" name="forgot_password_phrase"  required>
-        </div>
-        <div class="form-group">
-          <label for="Country">Country:</label>
-          <input type="text" name="Country"  required>
+            <label for="email">Email:</label>
+            <input type="email" name="email">
         </div>
 
         <div class="form-group">
-          <label for="date_of_birth">Date of Birth:</label>
-          <input type="date" name="date_of_birth"  required>
+            <label for="password">Password:</label>
+            <input type="password" name="password">
         </div>
         <div class="form-group">
-          <label for="gender">Gender:</label>
-          <input type="radio" name="gender" id="male" value="male" required>
-          <label for="male">Male</label> 
-          <input type="radio" name="gender" id="female" value="female" required>
-          <label for="female">Female</label>  
-          <input type="radio" name="gender" id="other" value="other" required>
-          <label for="other">Other</label>  
+            <label for="confirm_password">Confirm Password:</label>
+            <input type="password" name="confirm_password">
+        </div>
+        <div class="form-group">
+            <label for="forgot_password_phrase">Forgot password phrase:</label>
+            <input type="text" name="forgot_password_phrase">
+        </div>
+        <div class="form-group">
+            <label for="Country">Country:</label>
+            <input type="text" name="Country">
+        </div>
+
+        <div class="form-group">
+            <label for="date_of_birth">Date of Birth:</label>
+            <input type="date" name="date_of_birth">
+        </div>
+        <div class="form-group">
+            <label for="gender">Gender:</label>
+            <input type="radio" name="gender" id="male" value="male">
+            <label for="male">Male</label>
+            <input type="radio" name="gender" id="female" value="female">
+            <label for="female">Female</label>
+            <input type="radio" name="gender" id="other" value="other">
+            <label for="other">Other</label>
         </div>
 
         <div class="form-group center">
-          <button type="submit">Create Account</button>
+            <button type="submit" name="submit">Create Account</button>
         </div>
         <div class="form-group center">
-          <a href="login1.php">Already have an account</a>
+            <a href="login1.php">Already have an account</a>
         </div>
-      </form>
+    </form>
 </body>
 
 </html>
